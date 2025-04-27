b494557bfeef8f58696aaba6241f6951ce1f1ab4
Shivam Nikhra <142673389+ShivamNikhra14@users.noreply.github.com>
2025-04-27 18:03:32 +0530

    Adding Database integration

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDk-ej6vTCN4lyCXlpnARK2o3AjAuhtieg",
+  authDomain: "safevoice-1a7e9.firebaseapp.com",
+  projectId: "safevoice-1a7e9",
+  storageBucket: "safevoice-1a7e9.firebasestorage.app",
+  messagingSenderId: "317175431697",
+  appId: "1:317175431697:web:400eb8f707f9061e93a7b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
